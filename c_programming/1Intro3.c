7698e88f5f95be3dc919a19858b0c06c30318da1
Mohammad Mukhtaruzzaman <mukhtar@ramapo.edu>
2026-09-10 23:22:35 -0400

    Update 1Intro3.c

--- c_programming/1Intro3.c
+++ c_programming/1Intro3.c
@@ -17,3 +17,16 @@ int main(int argc, char **argv)
     return 0;
 }
 
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int i;
+
+    printf("Number of command-line arguments: %d\n", argc);
+    
+    for (i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
