0b60b2191d24c996995c1cc4bf4c709234a89f64
Mohammad Mukhtaruzzaman <mukhtar@ramapo.edu>
2026-09-10 22:55:43 -0400

    Update 1intro2.c

--- c_programming/1intro2.c
+++ c_programming/1intro2.c
@@ -4,3 +4,7 @@ int main() {
   printf("C-style new line!\n\n");  //new line
   return 0;
 }
+
+// \t	Creates a horizontal tab	
+// \\	Inserts a backslash character (\)	
+// \"	Inserts a double quote character
