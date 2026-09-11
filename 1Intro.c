14cc0d54254bfd847c739f49982a0c85944d1fbf
Mohammad Mukhtaruzzaman <mukhtar@ramapo.edu>
2026-09-10 22:28:56 -0400

    Create 1Intro.c

---
+++ 1Intro.c
@@ -0,0 +1,11 @@
+//The first program in C
+#include <stdio.h>
+
+int main() {
+  printf("I am now a C programmer!");
+  return 0;
+}
+
+//To compile and run any program:
+//To compile: gcc filename.c -o finename
+//To execute: ./filename
